77ada143ec3f709f6d301030b9e0c12c352bedbe
william <ingrid@ingrid-PC.(none)>
2012-07-15 19:37:11 -0700

    Surrey Christian Alliance Church

--- testdb.php
+++
@@ -1,8 +0,0 @@
- <?php 
-$link = mysql_connect('williamjxj.ipowermysql.com', 'church_test', 'williamjxj'); 
-if (!$link) { 
-    die('Could not connect: ' . mysql_error()); 
-} 
-echo 'Connected successfully'; 
-mysql_select_db(church); 
-?> 
